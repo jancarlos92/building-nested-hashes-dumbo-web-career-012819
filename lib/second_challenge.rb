116bab7af57a6f4e1fde1e67d1ff79b24979d53c
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-15 01:38:14 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,12 +1,12 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {:patriach => {}, 
-    :matriach => {}, 
+    :montague => {:patriarch => {}, 
+    :matriarch => {}, 
 :hero => {}, 
 :hero_friends => []      #your key/value pairs here!
       } , 
-    :capulet => { :patriach => {} ,
-    :matriach => {} ,
+    :capulet => { :patriarch => {} ,
+    :matriarch => {} ,
     :heroine => {} ,
     :heroine_friends => []
       }
